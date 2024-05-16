1ad6d4acce7848d9ad7003bc612066d88dbd645d
Felipe Pinheiro <felipehyo@gmail.com>
2024-05-16 19:31:36 -0300

    Ajustes no swagger de exemplo

--- src/main/java/com/postech/adapter/driver/api/controller/SwaggerController.java
+++ src/main/java/com/postech/adapter/driver/api/controller/SwaggerController.java
@@ -1,6 +1,5 @@
 package com.postech.adapter.driver.api.controller;
 
-import com.postech.core.domain.dto.ClientDTO;
 import com.postech.core.domain.dto.ErrorDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -10,7 +9,9 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/swagger", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -20,22 +21,12 @@ public class SwaggerController {
     //Swagger: http://localhost:8080/cafeteria/swagger-ui
     //Swagger yml: http://localhost:8080/cafeteria/api-docs
 
-    @Operation(summary = "Create test example", method = "POST")
+    @Operation(summary = "Test example", method = "GET", description = "This api is for example only")
     @ApiResponses(value = {
-            @ApiResponse(description = "Successful Operation", responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ClientDTO.class))),
+            @ApiResponse(description = "Successful Operation", responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class))),
             @ApiResponse(responseCode = "400", description = "Error example", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDTO.class))),
     })
-    @PostMapping(value = "/success", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> successTest(@RequestBody ClientDTO test) {
-        return ResponseEntity.ok(test);
-    }
-
-    @Operation(summary = "Create test example", method = "GET")
-    @ApiResponses(value = {
-            @ApiResponse(description = "Successful Operation", responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ClientDTO.class))),
-            @ApiResponse(responseCode = "400", description = "Error example", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDTO.class))),
-    })
-    @GetMapping(value = "/error")
+    @GetMapping
     public ResponseEntity<?> errorTest() {
         return ResponseEntity.badRequest().body(new ErrorDTO("400", "Error example"));
     }
