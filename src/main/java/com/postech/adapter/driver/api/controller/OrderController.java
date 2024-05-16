fdb2e35f7fafc087741a0b9aa481291df5c6f510
alyssonro <alysson.rodrigues@duxus.com.br>
2024-05-15 23:02:33 -0300

    add controllers

---
+++ src/main/java/com/postech/adapter/driver/api/controller/OrderController.java
@@ -0,0 +1,18 @@
+package com.postech.adapter.driver.api.controller;
+
+import com.postech.core.application.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @Autowired
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+}
